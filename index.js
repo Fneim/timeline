02ee1a57aaefbe7d8fb0feede387e55bd3df4477
fneim <neimfargin@gmx.com.tr>
2019-08-12 23:45:20 -0700

    delete button

--- index.js
+++ index.js
@@ -9,12 +9,13 @@ $(document).ready(function() {
       var eventContainer = $(".flex-container");
   
       // create new elements
-      var newDiv = $('<div id="item' + count + '" class="flex-item"></div>');
+      var newDiv = $('<div class="flex-item"></div>');
       var title = $("<h3>Title</h3>");
       var saveSpan = $('<span class="save-button" onclick=save(this)>Save</span>');
-      var tags = $('<div class="tags"><span>some tags</span></div>');
-      var textField = $('<input type="text" />');
+      var tags = $('<div class="tags"><p>some tags</p></div>');
+      var textField = $('<textarea type="text" placeholder="What happened next?..."></textarea>');
       var editSpan = $('<span class="edit-button" onclick=edit(this)>Edit</span>');
+      var deleteSpan = $('<span class="delete-button" onclick="deleteBtn(this)">Delete</span>');
   
       //append elements to html
       newDiv.append(title);
@@ -22,6 +23,7 @@ $(document).ready(function() {
       newDiv.append(tags);
       newDiv.append(textField);
       newDiv.append(editSpan);
+      newDiv.append(deleteSpan);
       eventContainer.append(newDiv);
     });
     
@@ -35,14 +37,23 @@ $(document).ready(function() {
     var parentElm = $(e).parent();
     $(e).fadeOut(1000).css("display", "none");
     parentElm.animate({"min-height": "300px"});
+    parentElm.children().eq(3).animate({"min-height": "250px"});
     //fading the parent's last child
     parentElm.children().eq(1).fadeIn(1000).css("display", "inline-block");
+    parentElm.children().last().fadeIn(1000).css("display", "inline-block");
   };
 
   // collapse flex-item with save
   const save = e => {
     var parentElm = $(e).parent();
     $(e).fadeOut(1000).css("display", "none");
+    parentElm.children().last().fadeOut(1000).css("display", "none");
     parentElm.animate({ "min-height":"0"});
-    parentElm.children().last().fadeIn(1000).css("display", "inline-block");
+    parentElm.children().eq(3).animate({"min-height": "0px"});
+    parentElm.children().eq(4).fadeIn(1000).css("display", "inline-block");
   };
+  
+  //   delete the event
+  const deleteBtn = e => {
+    e.closest('div').remove();
+  }
