e3620cf70378a0415b255237cdf1a383d78ef56e
fneim <neimfargin@gmx.com.tr>
2019-08-15 00:03:04 -0700

    added sparks logic for linking characters, tweaked css

--- sparks.js
+++ sparks.js
@@ -3,5 +3,9 @@ $(document).ready(function() {
 });
 
 const linkCharacter = () => {
-    
+    var character = $("#link-characters").val();
+    var characterSpan = $( `<span><i class="fas fa-user-circle">${character}</i></span>`);
+    console.log(character);
+    $("#characters-container div").append(characterSpan);
+    $("#link-characters").prop("selected", false);
 }
